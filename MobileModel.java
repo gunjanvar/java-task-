beb4277259bf332a798ca2547b053a706791b1d2
gunjan varshney <98642307+gunjanvar@users.noreply.github.com>
2022-09-01 21:01:46 +0530

    Add files via upload

---
+++ MobileModel.java
@@ -0,0 +1,23 @@
+public class MobileModel {
+	int eid,ePrice;
+	String eName, eCompany, eModel;
+	
+	public MobileModel(int eid, int ePrice, String eName, String eCompany, String eModel) {
+		this.eid = eid;
+		this.ePrice = ePrice;
+		this.eName = eName;
+		this.eCompany = eCompany;
+		this.eModel = eModel;
+	}
+
+	public int getePrice() {
+		return ePrice;
+	}
+
+	public void setePrice(int ePrice) {
+		this.ePrice = ePrice;
+	}
+
+	
+
+}
